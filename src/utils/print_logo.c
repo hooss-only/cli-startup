2c909c2fc640e59299bb501281e4a9d8ea303be3
hooss-only <hoossonly@gmail.com>
2024-09-15 12:16:21 +0900

    Added an error handling of not found 'logo' file
    Now, the program will make a file when there is no logo file in the config.

--- src/utils/print_logo.c
+++ src/utils/print_logo.c
@@ -2,8 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+const char *LOGO_PATH = "/.config/cli-startup/logo";
+
+void create_logo_file() {
+  char *path;
+  path = malloc(strlen(getenv("HOME")) + strlen(LOGO_PATH) + 1);
+  strcpy(path, getenv("HOME"));
+  strcat(path, LOGO_PATH);
+
+  FILE *fp = fopen(path, "a");
+
+  fputs(" ██████╗██╗     ██╗      ███████╗████████╗ █████╗ ██████╗ ████████╗██╗   ██╗██████╗ \n", fp);
+  fputs("██╔════╝██║     ██║      ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██╔══██╗\n", fp);
+  fputs("██║     ██║     ██║█████╗███████╗   ██║   ███████║██████╔╝   ██║   ██║   ██║██████╔╝\n", fp);
+  fputs("██║     ██║     ██║╚════╝╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██║   ██║██╔═══╝ \n", fp);
+  fputs("╚██████╗███████╗██║      ███████║   ██║   ██║  ██║██║  ██║   ██║   ╚██████╔╝██║     \n", fp);
+  fputs(" ╚═════╝╚══════╝╚═╝      ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     \n", fp);
+  
+  fclose(fp);
+  free(path);
+}
+
 void print_logo(char *color_code) {
-  const char *LOGO_PATH = "/.config/cli-startup/logo";
+  printf("%s\n", color_code);
   char *path;
   path = malloc(strlen(getenv("HOME")) + strlen(LOGO_PATH) + 1);
   strcpy(path, getenv("HOME"));
@@ -12,8 +33,9 @@ void print_logo(char *color_code) {
   FILE *fp = fopen(path, "r");
 
   if (fp == NULL) {
-    fprintf(stderr, "Not found: %s\n", path);
-    exit(1);
+    create_logo_file();
+    print_logo(color_code);
+    return;
   }
    
   long size;
